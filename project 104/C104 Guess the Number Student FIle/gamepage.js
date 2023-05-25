0f61003134d4a8efd878df91f5f34d1074922495
shrey07052010 <118438911+shrey07052010@users.noreply.github.com>
2023-05-25 15:51:24 +0530

    Add files via upload

---
+++ project 104/C104 Guess the Number Student FIle/gamepage.js
@@ -0,0 +1,44 @@
+// generation of random value
+
+var y = Math.floor(Math.random() * 10 + 1);
+
+playername = localStorage.getItem("player_name");
+
+var guess = 1;
+
+function submit()
+{
+
+
+
+// count of attempts
+// until hit
+  
+// function for the number sent by the user
+
+var x = document.getElementById("guessField").value;
+
+if( x == y )
+{
+    alert("CONGRATULATIONS!!!" + playername + "YOU GUESSED IT RIGHT IN " + guess + "GUESS");
+    guess = 1;
+}
+
+
+    else if(x > y)
+    {
+        guess++;
+        alert("OOPS SORRY!!! TRY A SMALLER NUMBER");
+    }
+    else
+    {
+        guess++;
+        alert("OOPS SORRY!!! TRY A GREATER NUMBER");
+    }
+}
+
+    function playAgain()
+    {
+        y = Math.floor(Math.random() * 10 + 1);
+    }
+
---
+++ project 104/C104 Guess the Number Student FIle/login.js
@@ -0,0 +1,9 @@
+// Create the login function here.
+function Login()
+{
+    player_name = document.getElementById("playername").value;
+    localStorage.setItem("player_name", player_name);
+
+    window.location = "gamepage.html";
+}
+
